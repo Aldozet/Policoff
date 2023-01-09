895dedf23dfbcb7da0f616bfd2b8297995bfea7b
Aldo Velazquez <aldo2897@gmail.com>
2023-01-09 17:26:37 -0600

    ya sube imagenes

--- phpBack/con_db.php
+++ phpBack/con_db.php
@@ -3,11 +3,11 @@
     $conex = new mysqli("localhost", "root","","policoffv2","3306");
     $conex->set_charset("utf8");
 
-  /*  if (!$conex) {
+    if (!$conex) {
 
         die("Error de conexión: " . mysqli_connect_error());
     } else {
 
         echo "Conexión exitosa";
-    }*/
+    }
 ?>
--- phpBack/regProducto.php
+++ phpBack/regProducto.php
@@ -7,24 +7,35 @@ if (isset($_POST['Enviar'])) {
     if (!empty($_POST['DescripcionProducto']) &&
         !empty($_POST['Ingredientes']) &&
         !empty($_POST['categoria']) &&
-        !empty($_POST['Precio']) &&
-        !empty($_POST['imagen'])
+        !empty($_POST['Precio']) 
     ) {
         $idproducto = null;
         $descripcion = trim($_POST['DescripcionProducto']);
         $ingredientes= trim($_POST['Ingredientes']);
         $categoria = trim($_POST['categoria']);
         $precio = trim($_POST['Precio']);
-        $imagen = trim($_POST['imagen']);
+
+
+        if (count($_FILES) > 0) {
+
+            if (is_uploaded_file($_FILES['imagen']['tmp_name'])) {
+                $imgData = file_get_contents($_FILES['imagen']['tmp_name']);
+                $imagen  = $_FILES['imagen']['type'];
+                
+                }
+        }else {
+
+            $imagen = null;
+        }
+
 
 
         // Verifica que la conexión se haya realizado correctamente
         if ($conex !== FALSE)
         {
             // Prepara la sentencia SQL
-            $$stmt = mysqli_stmt_init($conex);
-            if (mysqli_stmt_prepare($stmt,
-            "INSERT INTO `producto`(`idProducto`, `nombre`, `imagen`, `precio`, `tipo`, `descripcion`) VALUES (?,?,?,?,?,?)")) {
+            $stmt = mysqli_stmt_init($conex);
+            if (mysqli_stmt_prepare($stmt,"INSERT INTO `producto`(`idProducto`, `nombre`, `imagen`, `precio`, `tipo`, `descripcion`) VALUES (?,?,?,?,?,?)")) {
                 // Vincula las variables a los parámetros de la sentencia
                 mysqli_stmt_bind_param($stmt, "isssss", $idproducto, $descripcion, $imagen, $precio, $categoria, $ingredientes);
                 mysqli_stmt_execute($stmt);
