1b82591b65bad1feb2da666113eca5b29195ddc7
Aldo Velazquez <aldo2897@gmail.com>
2023-01-08 21:37:05 -0600

    login
    Inicio de sesion de usuario funcionando

--- phpBack/login.php
+++ phpBack/login.php
@@ -1,28 +1,20 @@
 <?php
 
-// Verifica que la conexión se haya realizado correctamente
-if ($conex !== FALSE) {
-    // Prepara la sentencia SQL
-    $stmt = mysqli_stmt_init($conex);
-    if (mysqli_stmt_prepare($stmt, "SELECT * FROM `usuario` WHERE `nombreUsuario` = ? AND `password` = ?")) {
-        // Vincula las variables a los parámetros de la sentencia
-        mysqli_stmt_bind_param($stmt, "ss", $nombreUsuario, $password);
-        mysqli_stmt_execute($stmt);
-        $result = mysqli_stmt_get_result($stmt);
-        $num_rows = mysqli_num_rows($result);
-        mysqli_stmt_close($stmt);
+
+if (!empty($_POST["Enviar"])) {
+   if (empty($_POST["usuario"]) || empty($_POST["contraseñaUsuario"])) {
+        echo '<div class="alerta">Uno o mas campos estan vacios</div>';
+   } else {
+        $nombreUsuario = trim($_POST["usuario"]);
+        $password = trim($_POST["contraseñaUsuario"]);
+        $sql = $conex->query("SELECT `nombreUsuario`, `password` FROM `usuario` WHERE nombreUsuario = '$nombreUsuario' and password = '$password'");
+        if ($datos = $sql->fetch_object()) {
+            header("location: ..\paginas\perfilUsuario.php");
+        } else {
+            echo '<div class="alerta">Acceso denegado</div>';
+        }
     }
-    // Si se ha encontrado un registro, inicia sesión
-    if ($num_rows == 1) {
-        // Inicia sesión y redirige al usuario a la página protegida
-        session_start();
-        $_SESSION['logged_in'] = true;
-        echo '<div class="alerta">inicio se sesion correcto</div>';
-        header("../paginas/perfilUsuario.html");
-    } else {
-        // Si no se ha encontrado un registro, muestra un mensaje de error
-        echo '<div class="alerta">Nombre de usuario o contraseña incorrectos</div>';
-    }
+   
 }
 
 
